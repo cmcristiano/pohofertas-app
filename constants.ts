b98cf95878fd53441dc9106762da025af13cf88b
Cristiano <31410010+cmcristiano@users.noreply.github.com>
2025-12-27 14:34:54 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -163,6 +163,102 @@ export const PRODUCTS: Product[] = [
     discount: 0,
     link: 'https://www.awin1.com/cread.php?awinmid=79926&awinaffid=2678244&ued=https%3A%2F%2Fwww.adidas.com.br%2Ftenis-campus-00s%2FIF8773.html'
   },
+  {
+    id: '1766856388841',
+    title: 'Notebook Acer Aspire 5 A515-45-R043 AMD Rayzen 5 Tela 15.6” 16 GB RAM 512 GB SSD Full HD LED IPS Windows 11 Home',
+    oldPrice: 4199,
+    newPrice: 2924.11,
+    image: 'https://m.media-amazon.com/images/I/61lLvuwZaKL._AC_SY355_.jpg',
+    store: 'Amazon',
+    category: 'tech',
+    validity: '2026-12-27',
+    discount: 30,
+    link: 'https://amzn.to/4pSNEJH'
+  },
+  {
+    id: '1766856464858',
+    title: 'Mustela Stelatopia+ Hidratante Relipidante Antiprurido 300 ml - Hidratação Imediata e Profunda para Peles Secas e Extremamente Secas Seguro Para Bebês, Crianças e Adultos - Sem Fragrância - Stelatopia',
+    oldPrice: 218.46,
+    newPrice: 104.4,
+    image: '',
+    store: 'Amazon',
+    category: 'beleza',
+    validity: '2026-12-27',
+    discount: 52,
+    link: 'https://amzn.to/3YeQQDl'
+  },
+  {
+    id: '1766856530726',
+    title: 'Brinox - Jogo de Panelas 6 Peças com Fundo de Indução Ceramic Life Botanika - Verde',
+    oldPrice: 760.04,
+    newPrice: 388.55,
+    image: 'https://m.media-amazon.com/images/I/41ABYLUvMTL._AC_SY450_.jpg',
+    store: 'Amazon',
+    category: 'cozinha',
+    validity: '2026-12-27',
+    discount: 49,
+    link: 'https://amzn.to/3La5A3w'
+  },
+  {
+    id: '1766856579868',
+    title: 'Mustela Solares Protetor Solar Infantil Loção Rosto E Corpo Fps 50+ 100 Ml',
+    oldPrice: 110.8,
+    newPrice: 80.5,
+    image: 'https://m.media-amazon.com/images/I/51pMOGNcmeL._AC_SY355_.jpg',
+    store: 'Amazon',
+    category: 'beleza',
+    validity: '2026-12-27',
+    discount: 27,
+    link: 'https://amzn.to/4azYb82'
+  },
+  {
+    id: '1766856644258',
+    title: 'Ventilador de Teto com Controle Remoto, LED 20W, Silencioso, 127-220V, Durável e Confiável – Branco',
+    oldPrice: 439.99,
+    newPrice: 300,
+    image: 'https://m.media-amazon.com/images/I/51FhU0hpSzL._AC_SY450_.jpg',
+    store: 'Amazon',
+    category: 'casa',
+    validity: '2026-12-27',
+    discount: 32,
+    link: 'https://amzn.to/4aFlU6R'
+  },
+  {
+    id: '1766856717068',
+    title: 'roborock Aspirador e esfregão robô Qrevo S5V, esfregão de borda FlexiArm, sucção de 12.000Pa, sistema duplo de emaranhamento zero, prevenção de obstáculos inteligente, elevação de esfregão de 10 mm',
+    oldPrice: 9073.35,
+    newPrice: 5660.11,
+    image: 'https://m.media-amazon.com/images/I/51Br5OrimUL._AC_SY450_.jpg',
+    store: 'Amazon',
+    category: 'casa',
+    validity: '2026-12-27',
+    discount: 38,
+    link: 'https://amzn.to/4jjcdx2'
+  },
+  {
+    id: '1766856782020',
+    title: 'Frigideira Antiaderente 24cm Com Revestimento Cerâmico, Grantino, Cabo de Madeira, Alta Resistência E Durabilidade, Compatível com Fogão Gás, Indução, Elétrico, Cooktop',
+    oldPrice: 78.9,
+    newPrice: 46,
+    image: 'https://m.media-amazon.com/images/I/61lwd9pJL8L._AC_SY450_.jpg',
+    store: 'Amazon',
+    category: 'casa',
+    validity: '2026-12-27',
+    discount: 42,
+    link: 'https://amzn.to/4qmwoMO'
+  },
+  {
+    id: '1766856839675',
+    title: 'Stanley - Copo de cerveja isolado, copo empilhável Stay Chill de 473 ml, para bebidas quentes ou frias',
+    oldPrice: 165,
+    newPrice: 73.38,
+    image: 'https://m.media-amazon.com/images/I/41Q4HoPivfL._AC_SX425_.jpg',
+    store: 'Amazon',
+    category: 'casa',
+    validity: '2026-12-27',
+    discount: 56,
+    link: 'https://amzn.to/3La5Rn4'
+  },
 
 
  ];
